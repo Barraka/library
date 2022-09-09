1d9ca88719d959b0dd469c9f0205a167834278ef
Emmanuel <girault.e@gmail.com>
2022-09-09 16:03:28 +0200

    Final project, with toggle buttons for read status

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//------------- form behaviour -------------
 let title=document.querySelector("#title");
 let author=document.querySelector("#author");
 let pages=document.querySelector("#pages");
@@ -13,13 +12,16 @@ let cancel=document.querySelector("#notadd");
 let modalbg=document.querySelector(".modalbg");
 let delbook=document.querySelector(".delbook");
 let delno=document.querySelector("#delno");
+let delyes=document.querySelector("#delyes");
 submit.addEventListener("click", checkinputs);
 up.addEventListener('click', scrollup);
 add.addEventListener('click', toggleAdd);
 cancel.addEventListener('click', toggleAdd);
-delno.addEventListener('click',toggledel);
+delno.addEventListener('click',toggledelmodal);
+delyes.addEventListener('click',removebook);
 modalbg.classList.toggle('modalbg');
 delbook.classList.toggle('delhide');
+let storeTargetToRemove;
 
 function makeBook(title,author,pages,isread) {
     this.title=title;
@@ -45,12 +47,7 @@ function outfocus(e) {
     
     else e.target.classList.remove("invalid");
 }
-// let myinput = prompt("Book list:");
 let myLibarary=[];
-myLibarary.push(
-{title: "The Hobbit", author: "J.R.R. Tolkien", pages: '546', isread: true},
-{title: "Peter Rabbit", author: "Not Me", pages: '54', isread: false}
-);
 
 function checkinputs() {
     
@@ -113,29 +110,48 @@ function generateCard(x) {
     author.textContent=x.author;
     let isread=document.createElement('div');
     isread.classList.add('check');
-    if(x.isread) {
-        let svg=document.createElement('div');
-        svg.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="check"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg>`
-        isread.appendChild(svg);
-    }
-    let cancel=document.createElement('div');
-    cancel.classList.add('hide');
-    cancel.id="cancel";
-    let delbtn=document.createElement('div');
-    delbtn.classList.add('delbtn');
-    delbtn.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="svg"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="9" y1="9" x2="15" y2="15"></line><line x1="15" y1="9" x2="9" y2="15"></line></svg>`;
-    cancel.appendChild(delbtn);
+    
+    let svg=document.createElement('div');
+    svg.innerHTML=`<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg>`
+    isread.appendChild(svg);
+    if(x.isread)isread.classList.add('checkgreen');
+    else isread.classList.add('checkgray');
+
+    let bookhover=document.createElement('div');
+    bookhover.id="bookhover";
+    let markread=document.createElement('div');
+    markread.classList.add('markread');
+    markread.innerHTML=`Mark as read:<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="bookhovercheck"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg>`
+    let deldiv=document.createElement('div');
+    deldiv.classList.add('bookhoverdelbtn');
+    deldiv.innerHTML=`Delete book?<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="delbooksvg"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="9" y1="9" x2="15" y2="15"></line><line x1="15" y1="9" x2="9" y2="15"></line></svg>`;
+    if(x.isread)markread.classList.add('hoverisread');
+    else markread.classList.add('hoverisnotread');
+    bookhover.appendChild(markread);
+    bookhover.appendChild(deldiv);
     container.appendChild(title);
     container.appendChild(author);
     container.appendChild(isread);
-    container.appendChild(cancel);
-    container.addEventListener('mouseover',toggleCancle);
-    container.addEventListener('mouseout',toggleCancle);
+    container.appendChild(bookhover);
+    container.setAttribute('data-id',myLibarary.length-1);
+    let delbtn=deldiv.querySelector('svg');
     delbtn.addEventListener('click',toggledel);
+    let checkmark=markread.querySelector('svg');    
+    checkmark.addEventListener('click',toggleisread);
     display.appendChild(container);    
 }
+myLibarary.push(
+    {title: "Mindset: The New Psychology of Success", author: "Carol S. Dweck ", pages: '320', isread: true}
+    );
 generateCard(myLibarary[0]);
+myLibarary.push(
+    {title: "Javascript: The Definitive Guide", author: "David Flanagan", pages: '1096', isread: false}
+    );
 generateCard(myLibarary[1]);
+myLibarary.push(
+    {title: "DOM Enlightenment", author: "Cody Lindley", pages: '181', isread: true}
+    );
+generateCard(myLibarary[2]);
 
 function scrollup() {
     window.scrollTo(0,0);
@@ -151,13 +167,52 @@ function eraseform() {
     author.value="";
     pages.value="";
 }
-function toggleCancle(e){
-    e.currentTarget.lastElementChild.classList.toggle("hide");
-}
-// function delbook(e){
-//     modalbg.classList.toggle('modalbg');
-// }
-function toggledel(){
+
+function toggledelmodal() {
     modalbg.classList.toggle('modalbg');
     delbook.classList.toggle('delhide');
+}   
+function toggledel(e){
+    storeTargetToRemove=e;
+    let info=document.querySelector('.infodelete')
+    let indexToDelete = e.target.parentElement.parentElement.getAttribute('data-id')
+    let currenttitle = e.currentTarget.parentElement.parentElement.parentElement.querySelector('.title').textContent;
+    let currentauthor = e.currentTarget.parentElement.parentElement.parentElement.querySelector('.author').textContent
+    info.innerHTML = '"'+currenttitle+'"<br> by <br>' + currentauthor;
+    modalbg.classList.toggle('modalbg');
+    delbook.classList.toggle('delhide');
+}
+function removebook(){
+    let target=storeTargetToRemove.target.parentElement.parentElement.parentElement.getAttribute('data-id');
+    target=parseInt(target);
+    myLibarary.splice(target,1);
+    let divtoremove=document.querySelector(`.book[data-id="${target}"]`);
+    divtoremove.remove();
+    storeTargetToRemove="";
+    toggledelmodal();
+}
+function toggleisread(e){
+    //toggle visibility
+    if(e.currentTarget.parentElement.classList.contains('hoverisread')) {
+        e.currentTarget.parentElement.classList.remove('hoverisread');
+        e.currentTarget.parentElement.classList.add('hoverisnotread');
+    }
+    else {
+        e.currentTarget.parentElement.classList.remove('hoverisnotread');
+        e.currentTarget.parentElement.classList.add('hoverisread');
+    }   
+
+    //changes in database
+    let currentIndex = e.currentTarget.parentElement.parentElement.parentElement.getAttribute('data-id');
+    myLibarary[currentIndex].isread=!myLibarary[currentIndex].isread;
+    //toggle cover visibility
+    let target = e.currentTarget.parentElement.parentElement.parentElement.querySelector('.check');
+    if(target.classList.contains('checkgreen')) {
+        target.classList.remove('checkgreen');
+        target.classList.add('checkgray');
+    }
+    else {
+        target.classList.remove('checkgray');
+        target.classList.add('checkgreen');
+    }
 }
